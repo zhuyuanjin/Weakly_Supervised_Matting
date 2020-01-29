a855ed2c1c52a041c7afcbeadd52eb8a774ca4aa
syxz <syxz1995@gmail.com>
2020-01-29 09:11:29 +0800

    first commit

--- main.py
+++ main.py
@@ -22,15 +22,21 @@ opt_RF = optim.SGD(RF.parameters(), lr=1e-3, momentum=0.9)
 a_path = '/home/zhuyuanjin/data/Human_Matting/alpha'
 img_path = '/home/zhuyuanjin/data/Human_Matting/image'
 
+ed_pretrained = '/home/zhuyuanjin/data/Human_Matting/models/ed_pretrained'
+rf_pretrained = '/home/zhuyuanjin/data/Human_Matting/models/rf_pretrained'
+
+final_param = '/home/zhuyuanjin/data/Human_Matting/models/final_param'
+
+
 dataset = MattingDataSet(a_path=a_path, img_path=img_path)
 dataloader = DataLoader(dataset, num_workers=10 , batch_size=batch_size, shuffle=True)
 
 if __name__ == '__main__':
 
     print('Beginning to PreTrain the Encoder Decoder')
-    if os.path.exists("ed_pretrained"):
-        print("loading param from %s" % "ed_pretrained")
-        state_dict = torch.load("ed_pretrained")
+    if os.path.exists(ed_pretrained):
+        print("loading param from %s" % ed_pretrained)
+        state_dict = torch.load(ed_pretrained)
         ED.load_state_dict(state_dict['net'])
         #opt_ED.load_state_dict(state_dict['optim'])
     
@@ -50,21 +56,21 @@ if __name__ == '__main__':
             loss_alpha = F.smooth_l1_loss(alpha_predict * unknown, alpha * unknown)
             loss = loss_alpha
             print(loss.item(), flush=True)
-            torch.save({'net':ED.state_dict(), 'optim':opt_ED.state_dict()}, 'ed_pretrained')
+            torch.save({'net':ED.state_dict(), 'optim':opt_ED.state_dict()}, ed_pretrained)
 #            total_loss += loss.item()
             opt_ED.zero_grad()
             loss.backward()
             opt_ED.step()
 #            if cnt % 100 == 0:
-#                torch.save(ED.state_dict(), "ed_pretrained")
+#                torch.save(ED.state_dict(), ed_pretrained)
 #                print("epoch", epoch,cnt * batch_size ,total_loss/100)
 #                total_loss = 0
 
     print('Beginning to PreTrain the RefineNet')
-    ED.load_state_dict(torch.load("ed_pretrained")['net'])
-    if os.path.exists("rf_pretrained"):
-        print("loading param from %s" % "rf_pretrained")
-        state_dict = torch.load("rf_pretrained")
+    ED.load_state_dict(torch.load(ed_pretrained)['net'])
+    if os.path.exists(rf_pretrained):
+        print("loading param from %s" % rf_pretrained)
+        state_dict = torch.load(rf_pretrained)
         RF.load_state_dict(state_dict['net'])
         #opt_RF.load_state_dict(state_dict['optim'])
 
@@ -78,7 +84,7 @@ if __name__ == '__main__':
             alpha_refined = RF(torch.cat((img, alpha_raw), 1))
             loss_refine = F.smooth_l1_loss(alpha_refined, alpha)
             print(loss_refine.item(), flush=True)
-            torch.save({'net': RF.state_dict(), 'optim': opt_RF.state_dict()}, 'rf_pretrained')
+            torch.save({'net': RF.state_dict(), 'optim': opt_RF.state_dict()}, rf_pretrained)
             opt_RF.zero_grad()
             loss_refine.backward()
             opt_RF.step()
@@ -96,7 +102,7 @@ if __name__ == '__main__':
             alpha_refined = RF(torch.cat((img, alpha_raw), 1))
             loss_refine = F.smooth_l1_loss(alpha_refined, alpha)
             print(loss_refine.item(), flush=True)
-            torch.save({'net_RF': RF.state_dict(), 'optim_RF': opt_RF.state_dict(), 'net_ED':ED.state_dict(), 'optim_ED':opt_ED.state_dict()},'final_param') 
+            torch.save({'net_RF': RF.state_dict(), 'optim_RF': opt_RF.state_dict(), 'net_ED':ED.state_dict(), 'optim_ED':opt_ED.state_dict()},final_param) 
             opt_RF.zero_grad()
             opt_ED.zero_grad()
             loss_refine.backward()
