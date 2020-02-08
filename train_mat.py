d8a6f8bd07c072d22d4c9ec415b0a9c2cec987f9
syxz1995 <syxz1995@gmail.com>
2020-02-08 08:14:09 +0800

    final version

--- train_mat.py
+++ train_mat.py
@@ -1,60 +1,63 @@
-from network import EncoderDecoder19
+from encoder_decoder import EncoderDecoder
 import torch.nn.functional as F
 import torch.optim as optim
 from DataSet import *
 import os
-from refinement import RefineNet
+from torch import nn
 
 
 device = 0 
 ed_epoch = 100
-batch_size = 10 
+batch_size = 8
+crop_size = (512, 512) 
+lrD = 1e-4
+lrE = 1e-5
+load_from_pretrain = True
+pretrain_decoder = True
 
-ED = EncoderDecoder19().double().cuda(device)
+ED= EncoderDecoder().double().cuda(device)
 
-opt_ED = optim.SGD(ED.parameters(), lr=1e-5, momentum=0.9)
+opt = optim.SGD(ED.parameters(), lr=lrE, )
+
+
 
 a_path = '/home/zhuyuanjin/data/Human_Matting/alpha'
 img_path = '/home/zhuyuanjin/data/Human_Matting/image_matting'
 
-ed_pretrained = '/home/zhuyuanjin/data/Human_Matting/models/param_Matting19'
+param_pretrained = "/home/zhuyuanjin/data/Human_Matting/models/ed_pretrained_saved"
+param = '/home/zhuyuanjin/data/Human_Matting/models/ed'
 
 
-
-dataset = MattingDataSet(a_path=a_path, img_path=img_path, )
+dataset = MattingDataSet(a_path=a_path, img_path=img_path, crop_size = crop_size)
 dataloader = DataLoader(dataset, num_workers=10 , batch_size=batch_size, shuffle=True)
 
 if __name__ == '__main__':
     print("Train model with size of dataset %d" % len(dataset))
-    print('Beginning to PreTrain the Encoder Decoder')
-#    if os.path.exists(ed_pretrained):
-#        print("loading param from %s" % ed_pretrained)
-#        state_dict = torch.load(ed_pretrained)
-#        ED.load_state_dict(state_dict['net'])
-        #opt_ED.load_state_dict(state_dict['optim'])
-    
+    print("Picture size of %d with batch size %d" % (crop_size[0], batch_size))
+
+
+    if os.path.exists(param_pretrained) & load_from_pretrain:
+        print("loading param from %s" % param_pretrained)
+        state_dict = torch.load(param_pretrained)
+        ED.load_state_dict(state_dict['net'])
+       #opt_ED.load_state_dict(state_dict['optim'])
+
+
+
     for epoch in range(ed_epoch):
-        cnt = 0
-        total_loss = 0
         for batch in dataloader:
-            cnt += 1
             img, alpha, trimap, unknown = batch['img'].cuda(device), \
                                               batch['alpha'].cuda(device), batch['trimap'].cuda(device), \
                                               batch['unknown'].cuda(device)
 
             input = torch.cat((img, trimap), 1)
             alpha_predict = ED(input)
+            loss_fn = nn.SmoothL1Loss()
             #img_predict = (fg * alpha_predict + bg * (1-alpha_predict)) * unknown
             #loss_comp = F.mse_loss(img_predict * unknown, img * unknown)
-            loss_alpha = F.smooth_l1_loss(alpha_predict * unknown, alpha * unknown)
-            loss = loss_alpha
+            loss = loss_fn(alpha_predict * unknown, alpha * unknown)
             print(loss.item(), flush=True)
-            torch.save({'net':ED.state_dict(), 'optim':opt_ED.state_dict()}, ed_pretrained)
-#            total_loss += loss.item()
-            opt_ED.zero_grad()
+            torch.save({'net':ED.state_dict(), 'optim':opt.state_dict()}, param)
+            opt.zero_grad()
             loss.backward()
-            opt_ED.step()
-#            if cnt % 100 == 0:
-#                torch.save(ED.state_dict(), ed_pretrained)
-#                print("epoch", epoch,cnt * batch_size ,total_loss/100)
-#                total_loss = 0
+            opt.step()
--- train_seg.py
+++ train_seg.py
@@ -1,30 +1,34 @@
-
-from network import EncoderDecoder19
+from network2 import Encoder, Decoder
 import torch.nn.functional as F
 import torch.optim as optim
 from DataSet import *
 import os
-from refinement import RefineNet
 from torch import nn
 
 device = 1 
 ed_epoch = 100
 refine_epoch = 100
 final_epoch = 100
-batch_size = 10 
+batch_size = 8 
+lrE = 5e-2
+lrD = 5e-2
+crop_size = (512, 512)
+load_from_pretrain = False
 
-ED = EncoderDecoder19().double().cuda(device)
+Encoder = Encoder().double().cuda(device)
+Decoder = Decoder().double().cuda(device)
 
-opt_ED = optim.SGD(ED.parameters(), lr=1e-2, momentum=0.9)
+optE = optim.SGD(Encoder.parameters(), lr=lrD, momentum=0.9)
+optD = optim.SGD(Decoder.parameters(), lr=lrE, momentum=0.9)
 
 a_path = '/home/zhuyuanjin/data/Human_Matting/alpha'
 img_path = '/home/zhuyuanjin/data/Human_Matting/image_seg'
 
-ed_pretrained = '/home/zhuyuanjin/data/Human_Matting/models/param_seg19'
+paramE = "/home/zhuyuanjin/data/Human_Matting/models/paramE_seg"
+paramD = '/home/zhuyuanjin/data/Human_Matting/models/paramD_seg'
 
 
-
-dataset = MattingDataSet(a_path=a_path, img_path=img_path, )
+dataset = MattingDataSet(a_path=a_path, img_path=img_path, crop_size=crop_size)
 dataloader = DataLoader(dataset, num_workers=10 , batch_size=batch_size, shuffle=True)
 
 if __name__ == '__main__':
@@ -36,28 +40,37 @@ if __name__ == '__main__':
 #        state_dict = torch.load(ed_pretrained)
 #        ED.load_state_dict(state_dict['net'])
 #        #opt_ED.load_state_dict(state_dict['optim'])
+    if os.path.exists(paramE) and load_from_pretrain:
+        print("Loading param from %s" % paramE)
+        state_dict = torch.load(paramE)
+        Encoder.load_state_dict(state_dict["net"])
+    
+
+    if os.path.exists(paramD) and load_from_pretrain:
+        print("Loading param from %s" % paramD)
+        state_dict = torch.load(paramD)
+        Decoder.load_state_dict(state_dict["net"])
+
     
     for epoch in range(ed_epoch):
-        cnt = 0
-        total_loss = 0
         for batch in dataloader:
-            cnt += 1
-            img, alpha, trimap, unknown, seg = batch['img'].cuda(device), \
-                                              batch['alpha'].cuda(device), batch['trimap'].cuda(device), \
-                                              batch['unknown'].cuda(device), batch["seg"].cuda(device)
+            img, trimap, seg = batch['img'].cuda(device), batch['trimap'].cuda(device), batch["seg"].cuda(device)
 
             input = torch.cat((img, trimap), 1)
-            seg_pred = ED(input)
+            features = Encoder(input)
+            seg_pred = Decoder(features) 
             #img_predict = (fg * alpha_predict + bg * (1-alpha_predict)) * unknown
             #loss_comp = F.mse_loss(img_predict * unknown, img * unknown)
-            loss_seg = F.binary_cross_entropy_with_logits(seg_pred, seg)
-            loss = loss_seg
+            loss = F.binary_cross_entropy_with_logits(seg_pred, seg)
             print(loss.item(), flush=True)
-            torch.save({'net':ED.state_dict(), 'optim':opt_ED.state_dict()}, ed_pretrained)
+            torch.save({'net':Encoder.state_dict(), 'optim':optE.state_dict()}, paramE)
+            torch.save({'net':Decoder.state_dict(), 'optim':optD.state_dict()}, paramD)
 #            total_loss += loss.item()
-            opt_ED.zero_grad()
+            optE.zero_grad()
+            optD.zero_grad()
             loss.backward()
-            opt_ED.step()
+            optE.step()
+            optD.step()
 #            if cnt % 100 == 0:
 #                torch.save(ED.state_dict(), ed_pretrained)
 #                print("epoch", epoch,cnt * batch_size ,total_loss/100)
